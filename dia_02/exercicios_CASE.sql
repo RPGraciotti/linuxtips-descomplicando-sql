5eff703411b94e9a510afa9a9ebda87a5e5dc3ac
rodolfo.graciotti@gmail.com <rodolfo.graciotti@gmail.com>
2022-12-15 23:35:40 +0000

    create exercicios case

--- dia_02/exercicios_CASE.sql
+++ dia_02/exercicios_CASE.sql
@@ -10,4 +10,16 @@ FROM
 
 -- COMMAND ----------
 
-
+-- Case 02:
+SELECT
+  *,
+  vlPreco + vlFrete AS vlTotal,
+  vlFrete / (vlPreco + vlFrete) AS pctFrete,
+  CASE
+    WHEN vlFrete / (vlPreco + vlFrete) <= 0.1 THEN '10%'
+    WHEN vlFrete / (vlPreco + vlFrete) <= 0.25 THEN '10% A 25%'
+    WHEN vlFrete / (vlPreco + vlFrete) <= 0.5 THEN '25% a 50%'
+    ELSE '+50%'
+  END AS DescFretePct
+FROM
+  silver_olist.item_pedido

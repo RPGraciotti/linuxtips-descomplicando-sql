cff17cb8811028ada03668454d0e10cb851dbf6c
rodolfo.graciotti@gmail.com <rodolfo.graciotti@gmail.com>
2022-12-16 00:14:50 +0000

    add having

---
+++ dia_02/having.sql
@@ -0,0 +1,30 @@
+-- Databricks notebook source
+SELECT
+  descUF,
+  COUNT(idVendedor)
+FROM
+  silver_olist.vendedor
+GROUP BY
+  descUF
+HAVING 
+  COUNT(idVendedor) >= 100
+
+-- COMMAND ----------
+
+SELECT
+  descUF,
+  COUNT(idVendedor) AS qtVendedorUF
+FROM
+  silver_olist.vendedor
+WHERE
+  descUF IN ('SP', 'MG', 'RJ', 'ES')
+GROUP BY
+  descUF
+HAVING
+  qtVendedorUF >= 100
+ORDER BY
+  qtVendedorUF DESC
+
+-- COMMAND ----------
+
+SHOW TABLES FROM silver_olist

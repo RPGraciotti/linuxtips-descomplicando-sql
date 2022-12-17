00a452c4d61268bb5d5d068dd58d622b72de80e9
rodolfo.graciotti@gmail.com <rodolfo.graciotti@gmail.com>
2022-12-17 01:01:00 +0000

    ident window_function

--- dia_04/window_functions.sql
+++ dia_04/window_functions.sql
@@ -12,26 +12,25 @@ WITH tb_vendas_vendedores AS (
     qtVendas DESC
 ),
 tb_row_number AS (
-SELECT
-  T1.*,
-  T2.descUF,
-  ROW_NUMBER() OVER(
-    PARTITION BY T2.descUF
-    ORDER BY
-      qtVendas DESC
-  ) AS RN
-FROM
-  tb_vendas_vendedores AS T1
-  LEFT JOIN silver_olist.vendedor AS T2 ON T1.idVendedor = T2.idVendedor
-  
-  QUALIFY RN <= 10
-  
-ORDER BY
-  descUF, 
-  qtVendas DESC
+  SELECT
+    T1.*,
+    T2.descUF,
+    ROW_NUMBER() OVER(
+      PARTITION BY T2.descUF
+      ORDER BY
+        qtVendas DESC
+    ) AS RN
+  FROM
+    tb_vendas_vendedores AS T1
+    LEFT JOIN silver_olist.vendedor AS T2 ON T1.idVendedor = T2.idVendedor QUALIFY RN <= 10
+  ORDER BY
+    descUF,
+    qtVendas DESC
 )
-
-SELECT * FROM tb_row_number
+SELECT
+  *
+FROM
+  tb_row_number
 
 -- COMMAND ----------
 
